acde0477ece3095892b6371ac0225de3aef32f52
현종훈 <hjh6219@gmail.com>
2019-07-01 15:20:37 +0900

    한 줄 추가 "Local"

--- src/main/java/git_gradle_test/HellowWorld.java
+++ src/main/java/git_gradle_test/HellowWorld.java
@@ -7,6 +7,7 @@ public class HellowWorld {
 		System.out.println("World's end Dancehall");
 		System.out.println("Earth's end Dancehall");
 		System.out.println("Hellow Dance World");
+		System.out.println("Local");
 	}
 
 }
